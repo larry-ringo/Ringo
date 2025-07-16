70156e1f8e37fcb18e8f26ad1020bcab7f80f0c1
arash-1819 <51744028+arash-1819@users.noreply.github.com>
2025-07-15 22:11:56 -0400

    Update HomeMobile.tsx

--- ringo/src/pages/HomeMobile.tsx
+++ ringo/src/pages/HomeMobile.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = process.env.REACT_APP_API_URL;
 
 export default function WebMenu() {
   const [embedLink, setEmbedLink] = useState<string | null>(null);
