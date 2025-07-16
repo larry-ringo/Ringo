b016757bde5f905f9e5ad8f94298933cc3fdc81e
arash-1819 <51744028+arash-1819@users.noreply.github.com>
2025-07-15 20:50:52 -0400

    NOW

---
+++ ringo/craco.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  eslint: {
+    enable: true,
+    configure: {
+      rules: {
+        "jsx-a11y/anchor-has-content": "off",
+      },
+    },
+  },
+};
+
--- ringo/src/pages/HomeMobile.tsx
+++ ringo/src/pages/HomeMobile.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import defaultImg from "../assets/home_mobile/default.png";
